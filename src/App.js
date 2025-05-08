c3206b62162e3b56e87346d7f1aa476df0318577
sol wu <apiswswsw@gmail.com>
2025-05-08 16:52:58 -0700

    revert to an example page instead of generated page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { App as SendbirdApp } from '@sendbird/uikit-react';
+import '@sendbird/uikit-react/dist/index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div style={{ width:'100vw', height:'100vh' }}>
+      {/* 
+        This super smart component serves as a drop-in chat solution
+        
+        For advanced 🚀 customizations, use SendbirdProvider:
+        https://sendbird.com/docs/chat/uikit/v3/react/essentials/sendbirdprovider
+      */}
+      <SendbirdApp
+        appId={'4E26DC08-4FDC-415E-B8E4-3F45CD24FC43'}
+        userId={'user3'}
+        accessToken={'ff43a32bc04767ad2d619129c4b8f0a0fbd7306c'} // Optional, but recommended
+      />
     </div>
-  );
+  )
 }
 
 export default App;
